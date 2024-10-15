653a03028952d316db023c3bdf09367d62aeb013
Maddie <madelen.sletteberg@gmail.com>
2024-10-15 15:19:24 +0200

    Revert "Updates eslint config file to integrate prettier"
    This reverts commit 74d7142902c0c75c58b84f9e25abffe6c814a0a7.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,32 +1,8 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
-import prettier from "eslint-plugin-prettier";
-import prettierConfig from "eslint-config-prettier";
 
 export default [
-  {
-    
-    languageOptions: { 
-      globals: globals.browser,
-    },
-  
-    extends: [
-      pluginJs.configs.recommended,
-      prettierConfig,
-    ], 
-
-    rules: {
-      ...prettier.rules,
-
-      "prettier/prettier": [
-       "error",
-       {
-        singleQuote: false,
-        semi: true,
-        trailingComma: "es5",
-       },
-     ],
-   },
- },
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
 ];
