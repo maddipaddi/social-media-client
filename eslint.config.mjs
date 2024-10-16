bad831f2c2c74969ea66f01f77aac73c63665699
Maddie <madelen.sletteberg@gmail.com>
2024-10-16 13:01:27 +0200

    Removes comments

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,24 +1,19 @@
 import globals from "globals";
-import pluginJs from "@eslint/js";
-import prettier from "eslint-plugin-prettier";
-import prettierConfig from "eslint-config-prettier";
 
 export default [
   {
-    // Define environment for both browser and Node.js
     languageOptions: {
       globals: {
         ...globals.browser,
-        ...globals.node,  // Include Node.js globals like 'require' and 'module'
+        ...globals.node,
       },
     },
   },
   {
-    // Specific config for Jest test files
-    files: ["**/*.test.js", "**/__tests__/**/*.js"],  // Apply this config to test files
+    files: ["**/*.test.js", "**/__tests__/**/*.js"],
     languageOptions: {
       globals: {
-        ...globals.jest,  // Jest globals like 'test' and 'expect'
+        ...globals.jest,
       },
     },
   },
