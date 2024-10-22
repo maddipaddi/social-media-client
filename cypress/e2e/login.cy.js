1bcbeefc5d02caceb1545bc3d254dd48f548b1e0
Maddie <madelen.sletteberg@gmail.com>
2024-10-22 13:07:46 +0200

    Adds e2e test to check that a user cannot login with invalid credentials and will be shown a message

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -13,4 +13,23 @@ describe("Login functionality", () => {
 
     cy.url().should("include", "/?view=profile&name=");
   });
+
+  it("should not submit the login form with invalid credentials and shows a message to the user", () => {
+    cy.visit("/");
+
+    cy.get(".btn.btn-outline-success.me-2").click();
+
+    cy.get("#loginForm").within(() => {
+      cy.get("#loginEmail").type("invalid-email-123@noroff.no");
+      cy.get("#loginPassword").type("invalid-password-123");
+
+      cy.get('button[type="submit"]').click();
+    });
+
+    cy.on("window:alert", (alertText) => {
+      expect(alertText).to.equal(
+        "Either your username was not found or your password is incorrect"
+      );
+    });
+  });
 });
